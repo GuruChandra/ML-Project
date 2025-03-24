a50e0f6f57a9238f082ffd21945f5d20d59e9a3e
GuruChandra <guruchandu4@gmail.com>
2025-03-24 07:47:57 +0530

    completed until evaluation

--- Text-Summarizer-Project/main.py
+++ Text-Summarizer-Project/main.py
@@ -1,6 +1,8 @@
 from textSummarizer.logging import logger
 from textSummarizer.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.stage02_data_validation import  DataValidationTrainingPipeline
+from textSummarizer.pipeline.stage03_data_tranformation import DataTransformationTrainingPipeline
+from textSummarizer.pipeline.stage04_model_trainer import ModelTrainingPipeline
 logger.info("Welcome to our custom logging...")
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -22,6 +24,31 @@ try:
     logger.info(f">>>> stage {STAGE_NAME} started <<<<<<<<<<<<")
     data_validation = DataValidationTrainingPipeline()
     data_validation.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<<<< \n\nx=========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Transformation Stage"
+
+try:
+    logger.info(f">>>> stage {STAGE_NAME} started <<<<<<<<<<<<")
+    data_tranformation = DataTransformationTrainingPipeline()
+    data_tranformation.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<<<< \n\nx=========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Model Training Stage"
+
+try:
+    logger.info(f">>>> stage {STAGE_NAME} started <<<<<<<<<<<<")
+    data_tranformation = ModelTrainingPipeline()
+    data_tranformation.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<<<< \n\nx=========x")
 except Exception as e:
     logger.exception(e)
